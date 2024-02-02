ced999e899ce1618a30a82df604c2ed2f57ca30f
Lucifer-Aspire-A315-21 <omkaromu183@gmail.com>
2024-02-02 08:11:50 +0530

    Home page Done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,7 @@ import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,21 +20,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}
-        <Topbar />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}
+          <Topbar />
 
-        <main>
-          <LeftSidebar />
-          <section className="main-container">
-            <div className="w-full max-w-4xl">
-              {children}
-            </div>
-          </section>
-          <RightSidebar />
-        </main>
-        <Bottombar />
-      </body>
-    </html>
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
